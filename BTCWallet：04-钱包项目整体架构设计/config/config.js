c3083b694518c9c431de3fa035e07fb7288102d7
李旭 <lixu@lixudeMacBook-Pro-4.local>
2018-10-29 13:52:23 +0800

    update

--- BTCWallet：04-钱包项目整体架构设计/config/config.js
+++ BTCWallet：04-钱包项目整体架构设计/config/config.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = {
     BWS_URL: 'https://bws.bitpay.com/bws/api',
     networkType: "testnet",//livenet,testnet
-    coinType: "bch",
+    coinType: "btc",
     copayerName: "lixu",
     walletFilePath: path.join(__dirname, "../static/wallet_file"),
 }
